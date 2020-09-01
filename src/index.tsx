7970e234a8113eacca0534fe1c8e713075e3e188
TakayukiTooyama <t.tooyama107@gmail.com>
2020-09-01 17:15:53 +0900

    material-ui & styled-components install

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { StylesProvider } from '@material-ui/styles';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StylesProvider injectFirst>
+      <App />
+    </StylesProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
