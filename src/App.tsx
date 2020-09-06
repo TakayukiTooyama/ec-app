d90a848c8d11ef52d97b68e0520f81fdb3e43e07
TakayukiTooyama <t.tooyama107@gmail.com>
2020-09-06 11:08:01 +0900

    style & theme追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Header } from './components/Header';
 import Router from './Router';
+import './assets/reset.css';
+import './assets/style.css';
 
 function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,11 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import * as History from 'history';
 import { StylesProvider } from '@material-ui/styles';
-// import { MuiThemeProvider } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
-import createStore from './reducks/store/store';
 import App from './App';
+import createStore from './reducks/store/store';
+import { theme } from './assets/theme';
 
 const history = History.createBrowserHistory();
 export const store = createStore(history);
@@ -16,9 +17,9 @@ render(
   <StylesProvider injectFirst>
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        {/* <MuiThemeProvider theme={theme}> */}
-        <App />
-        {/* </MuiThemeProvider> */}
+        <MuiThemeProvider theme={theme}>
+          <App />
+        </MuiThemeProvider>
       </ConnectedRouter>
     </Provider>
   </StylesProvider>,
