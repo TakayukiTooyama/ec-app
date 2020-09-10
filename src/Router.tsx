3f479ea414eddc853d025c9f8ef4220d7dbd3d12
TakayukiTooyama <t.tooyama107@gmail.com>
2020-09-10 14:22:27 +0900

    プロフィールページ作成

--- src/Router.tsx
+++ src/Router.tsx
@@ -12,6 +12,7 @@ import {
   CartList,
   OrderConfirm,
   OrderHistory,
+  UserMypage,
 } from './templates';
 
 const Router = () => {
@@ -24,9 +25,14 @@ const Router = () => {
       <Auth>
         <Route exact path={'(/)?'} component={ProductList} />
         <Route exact path={'/product/:id'} component={ProductDetail} />
-        {/* 新規追加と編集機能を区別するため */}
         <Route path={'/product/edit(/:id)?'} component={ProductEdit} />
+        {/* 新規追加と編集機能を区別するため */}
+
         <Route exact path={'/cart'} component={CartList} />
+
+        <Route exact path={'/user/mypage'} component={UserMypage} />
+        <Route exact path={'/user/payment/edit'} component={UserMypage} />
+
         <Route exact path={'/order/confirm'} component={OrderConfirm} />
         <Route exact path={'/order/history'} component={OrderHistory} />
       </Auth>
---
+++ src/templates/UserMypage.tsx
@@ -0,0 +1,35 @@
+import React, { useCallback } from 'react';
+import { TextDetail, Button } from '../components/UIkit';
+import { useSelector, useDispatch } from 'react-redux';
+import { getUsername } from '../reducks/users/selectors';
+import { Users } from '../reducks/users/types';
+import { push } from 'connected-react-router';
+
+const UserMypage = () => {
+  const dispatch = useDispatch();
+  const selector = useSelector((state: Users) => state);
+  const username = getUsername(selector);
+
+  const transition = useCallback(
+    (path) => {
+      dispatch(push(path));
+    },
+    [dispatch]
+  );
+
+  return (
+    <section className="c-section-container">
+      <h2 className="u-text__headline u-text-center">マイページ</h2>
+      <div className="module-spacer--medium"></div>
+      <TextDetail label="ユーザー名" value={username} />
+      <div className="module-spacer--medium"></div>
+      <div className="center">
+        <Button label="カード情報の編集" onClick={() => transition('/user/payment/edit')} />
+        <div className="module-spacer--extra-extra-small"></div>
+        <Button label="注文履歴の確認" onClick={() => transition('/order/history')} />
+      </div>
+    </section>
+  );
+};
+
+export default UserMypage;
